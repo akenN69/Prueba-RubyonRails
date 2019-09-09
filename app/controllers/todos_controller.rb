d210c23c4923ecd11cbe6ad75ea0f375f9d9ff28
Seba_virtual <rata_6952@hotmail.com>
2019-09-09 20:30:26 -0300

    add CRUD

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,6 @@
 class TodosController < ApplicationController
+    before_action :set_todo, only: [:show, :edit, :update, :destroy]
+
 
     def index 
         @todos = Todo.all
@@ -7,4 +9,40 @@ class TodosController < ApplicationController
     def new
         @todo = Todo.new
     end 
+
+    def create
+        todo = Todo.create(todo_params)
+        todo.completed = false
+        todo.save
+        redirect_to todos_path
+    end
+
+    def show
+        
+    end 
+
+    def edit
+       
+    end
+
+    def update
+        @todo.update(todo_params)
+        redirect_to todos_path
+        @todo = Todo.find(params[:id])
+    end
+
+    def destroy
+        @todo.destroy
+        redirect_to todos_path 
+
+    end
+
+    def set_todo
+        @todo = Todo.find(params[:id])
+    end
+
+    private
+    def todo_params
+        params.require(:todo).permit(:description, :completed)
+    end
 end
